c32f561518204b9cf1bdccd7bf7d98786ebce6d5
erickguerra22 <erickguerra2201@gmail.com>
2024-09-19 14:44:10 -0600

    Diffusion Particles as Class

--- DiffusionParticles.py
+++ DiffusionParticles.py
@@ -1,92 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from random import randint
 
-# Parámetros de la simulación
-M, N = 50, 50  # Tamaño del grid
-T = 100        # Tiempo total de simulación
-K = 0.5        # Parámetro de velocidad de difusión
-P = 100         # Número de partículase
-r = 26        # Altura de la región bloqueada
-s = 26         # Ancho de la región bloqueada
-mask = np.ones((M, N))
-Nexp = 100       # Número de experimentos
-
-if r is not None and s is not None:
-    mask[:r, s:] = 0
-
-initial_distribution = np.zeros((M, N))
-initial_distribution[25, 25] = 1.0
-initial_distribution[12, 25] = 1.0
-initial_distribution[40, 10] = 1.0
-initial_distribution[5, 5] = 1.0
-
-# Distribución inicial u(0)
-# u0 = np.random.rand(M, N)
-u0 = initial_distribution
-u0 /= u0.sum()  # Normalizar para que la suma sea 1
-
-# Función para realizar un experimento
-def simulate_diffusion(M, N, P, K, T):
-    particles = np.random.choice(M * N, size=P, p=u0.ravel())  # Distribuir partículas según u(0)
-    particle_positions = np.unravel_index(particles, (M, N))
-    density0 = np.zeros((M, N), dtype=int)
-    
-    for x, y in zip(particle_positions[0], particle_positions[1]):
-        density0[x, y] += 1
-
-    # Listas para guardar las densidades en el tiempo
-    densities = []
-    densities.append(density0)
-
-    # Movimientos posibles: 8 vecinos + quedarse en la misma posición
-    moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1), (0, 0)]
-
-    for t in range(T):
-        new_grid = np.zeros((M, N), dtype=int)
-        for i in range(M):
-            for j in range(N):
-                if mask[i, j] == 0:
-                    new_grid[i, j] = 0
-                    continue
-                if density0[i, j] > 0:
-                    for _ in range(density0[i, j]):
-                        if np.random.rand() < K:
-                            move = moves[np.random.randint(0, 8)]
-                            ni, nj = (i + move[0]) % M, (j + move[1]) % N  # Periodic boundary conditions
-                            new_grid[ni, nj] += 1
-                        else:
-                            new_grid[i, j] += 1
-        density0 = new_grid
-        densities.append(density0.copy())
-
-    return densities
-
-# Simulación y promedios
-average_density = np.zeros((M, N), dtype=float)
-
-for exp in range(Nexp):
-    densities = simulate_diffusion(M, N, P, K, T)
-    for t, density in enumerate(densities):
-        if exp == 0:
-            all_densities = np.array([density.copy() for _ in range(T + 1)], dtype='float')
+class DiffusionParticles:
+    def __init__(self, M, N, T, K, r=None, s=None, initial_distribution=None, Nexp=100, P= None):
+        self.M = M
+        self.N = N
+        self.T = T
+        self.K = K
+        self.r = r
+        self.s = s
+        self.Nexp = Nexp
+        self.P = P if P is not None else randint(1,2000)
+        self.mask = np.ones((M, N))
+        self.neighbors = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        
+        # Si se proporcionan r y s, establecer las celdas correspondientes en 0
+        if r is not None and s is not None:
+            self.mask[:r, s:] = 0
+        
+        # Si se proporciona una distribución inicial, se utiliza, de lo contrario se genera una aleatoria
+        if initial_distribution is not None:
+            self.u0 = initial_distribution
+            self.u0 /= np.sum(self.u0)  # Normalizar para que la suma sea 1
         else:
-            all_densities[t] += density
+            self.u0 = np.random.rand(M, N)
+            self.u0 /= np.sum(self.u0)  # Normalizar para que la suma sea 1
+            
+    def simulate_diffusion(self):
+        particles = np.random.choice(self.M * self.N, size=self.P, p=self.u0.ravel())  # Distribuir partículas según u(0)
+        particle_positions = np.unravel_index(particles, (self.M, self.N))
+        grid = np.zeros((self.M, self.N), dtype=int)
+        
+        for x, y in zip(particle_positions[0], particle_positions[1]):
+            grid[x, y] += 1
 
-# Normalizar
-all_densities /= (Nexp * P)
+        # Listas para guardar las densidades en el tiempo
+        densities = [grid.copy()]
 
-def animate(i):
-    plt.clf()  # Limpiamos el gráfico
-    plt.imshow(all_densities[i], cmap='gray', interpolation='nearest')
-    plt.title(f'Tiempo: {i}')
-    plt.colorbar()
+        for t in range(self.T):
+            new_grid = np.zeros((self.M, self.N), dtype=int)
+            for i in range(self.M):
+                for j in range(self.N):
+                    if self. mask[i, j] == 0:
+                        new_grid[i, j] = 0
+                        continue
+                    if grid[i, j] > 0:
+                        for _ in range(grid[i, j]):
+                            if np.random.rand() < self.K:
+                                move = self.neighbors[np.random.randint(0, 8)]
+                                ni, nj = (i + move[0]) % self.M, (j + move[1]) % self.N
+                                new_grid[ni, nj] += 1
+                            else:
+                                new_grid[i, j] += 1
+            grid = new_grid
+            densities.append(grid.copy())
+
+        return densities
     
-# Crear la animación
-fig = plt.figure()
-ani = animation.FuncAnimation(fig, animate, frames=T, interval=100)
+    # Simulación y promedios
+    def getSimulation(self):
+        all_densities = np.array([density.copy() for _ in range(self.T + 1)], dtype='float')
+        for exp in range(self.Nexp):
+            densities = self.simulate_diffusion(self)
+            for t, density in enumerate(densities):
+                if exp != 0:
+                    all_densities[t] += density
 
-ani.save('./animations/inciso_2/diffusion_simulation.mp4', writer='ffmpeg', fps=10)
+        # Normalizar
+        all_densities /= (self.Nexp * self.P)
+        return all_densities
 
-plt.show()
-
+    # Función para crear la animación de la simulación
+    def animate_simulation(self, densities):
+        def animate(i):
+            plt.clf()  # Limpiamos el gráfico
+            plt.imshow(densities[i], cmap='gray', interpolation='nearest')
+            plt.title(f'Tiempo: {i}')
+            plt.colorbar()
+            
+        # Crear la animación
+        fig = plt.figure()
+        ani = animation.FuncAnimation(fig, animate, frames=self.T, interval=100)
+        return ani
